c96b6900b8c0aaedcd7b2d190f48efdedf5f345f
Martin Kartawijaya <git@m.kartawijaya.dev>
2021-11-16 18:04:03 +0100

    Pull application specific code out of fsm.py and add better type hints

--- dango/dango.py
+++ dango/dango.py
@@ -3,14 +3,77 @@ from typing import List
 from sudachipy import dictionary
 from sudachipy.morpheme import Morpheme
 
-from .fsm import create_fsm
+from .fsm import StateMachine, State
 from .util import katakana_to_hiragana
 from .word import Word
 
 
+class WordState(State):
+
+    def __init__(self, name: str, start_new_word=False):
+        self.name = name
+        self.start_new_word = start_new_word
+
+    def on_input(self, context: List[List[Morpheme]], morpheme: Morpheme) -> None:
+        if self.start_new_word:
+            context.append([])
+        context[-1].append(morpheme)
+
+
+UNSPECIFIED_WORD_STATE = WordState('UNSPECIFIED', start_new_word=True)
+VERB_STATE = WordState('VERB', start_new_word=True)
+VERB_INFLECTION_STATE = WordState('VERB_INFLECTION')
+ADJECTIVE_STATE = WordState('ADJECTIVE', start_new_word=True)
+ADJECTIVE_INFLECTION_STATE = WordState('ADJECTIVE_INFLECTION')
+
+WORD_AGGREGATION_FSM = StateMachine(
+    [
+        UNSPECIFIED_WORD_STATE,
+        VERB_STATE,
+        VERB_INFLECTION_STATE,
+        ADJECTIVE_STATE,
+        ADJECTIVE_INFLECTION_STATE
+    ],
+    UNSPECIFIED_WORD_STATE,
+    UNSPECIFIED_WORD_STATE,
+    [
+        # --- transitions to aggregate inflected verbs ---
+
+        # start of a verb
+        (None, ('動詞', '一般', '*', '*'), VERB_STATE),
+        # Some verbs (for example 見る) are detected as non independent even when standing alone, so we also need
+        # account for these cases for starting a new verb. Since specific transitions have higher priority than
+        # wildcard transition we don't risk accidentally breaking apart an inflected verb apart.
+        (None, ('動詞', '非自立可能', '*', '*'), VERB_STATE),
+        # The inflected part of a verb is started by either an auxiliary verb, ...
+        (VERB_STATE, ('助動詞', '*', '*', '*'), VERB_INFLECTION_STATE),
+        # or a conjunctive particle, i.e. て or で.
+        (VERB_STATE, ('助詞', '接続助詞', '*', '*'), VERB_INFLECTION_STATE),
+        # continue aggregating any auxiliary verbs ...
+        (VERB_INFLECTION_STATE, ('助動詞', '*', '*', '*'), VERB_INFLECTION_STATE),
+        # and non independent verbs, e.g. the いる　of the continuous form
+        (VERB_INFLECTION_STATE, ('動詞', '非自立可能', '*', '*'), VERB_INFLECTION_STATE),
+
+        # suffix for seeming/looks-like
+        (VERB_STATE, ('形状詞', '助動詞語幹', '*', '*'), VERB_INFLECTION_STATE),
+
+        # --- transitions to aggregate inflected adjectives ---
+
+        # start of an adjective
+        (None, ('形容詞', '一般', '*', '*'), ADJECTIVE_STATE),
+        # can be followed by negating suffix
+        (ADJECTIVE_STATE, ('形容詞', '非自立可能', '*', '*'), ADJECTIVE_INFLECTION_STATE),
+        # and/or suffix for the past-tense
+        (ADJECTIVE_STATE, ('助動詞', '*', '*', '*'), VERB_INFLECTION_STATE),
+        (ADJECTIVE_INFLECTION_STATE, ('助動詞', '*', '*', '*'), VERB_INFLECTION_STATE),
+
+        # suffix for seeming/looks-like
+        (ADJECTIVE_STATE, ('形状詞', '助動詞語幹', '*', '*'), ADJECTIVE_INFLECTION_STATE)
+    ])
+
+
 class Tokenizer:
     def __init__(self):
-        self._fsm = create_fsm()
         self._dictionary = dictionary.Dictionary()
         self._tokenizer = self._dictionary.create()
 
@@ -31,7 +94,7 @@ class Tokenizer:
 
     def tokenize(self, phrase: str) -> List[Word]:
         morphemes = self._tokenizer.tokenize(phrase)
-        return [self.make_word(mm) for mm in self._fsm.run(morphemes)]
+        return [self.make_word(mm) for mm in WORD_AGGREGATION_FSM.run([], morphemes)]
 
 
 TOKENIZER = Tokenizer()
--- dango/fsm.py
+++ dango/fsm.py
@@ -1,120 +1,66 @@
-from typing import Iterable, List
+from typing import Iterable, List, Tuple, Optional, Any
 
 from sudachipy.morpheme import Morpheme
 
 
-class WordState(object):
-    def __init__(self, name, start_new_word=False):
-        self._name = name
-        self._start_new_word = start_new_word
+class State:
 
-    def on_input(self, context: List[List[Morpheme]], morpheme: Morpheme):
-        if self._start_new_word:
-            context.append([])
-        context[-1].append(morpheme)
-
-    def __str__(self):
-        return self._name
+    def on_input(self, context: Any, morpheme: Morpheme) -> None:
+        pass
 
 
-UNSPECIFIED_WORD_STATE = WordState('UNSPECIFIED', start_new_word=True)
-VERB_STATE = WordState('VERB', start_new_word=True)
-VERB_INFLECTION_STATE = WordState('VERB_INFLECTION')
-ADJECTIVE_STATE = WordState('ADJECTIVE', start_new_word=True)
-ADJECTIVE_INFLECTION_STATE = WordState('ADJECTIVE_INFLECTION')
+MorphemeFeatures = Tuple[str, str, str, str]
+
+StateTransitionRule = Tuple[Optional[State], MorphemeFeatures, State]
 
 
-def node_features(node: Morpheme):
-    return tuple(node.part_of_speech()[:4])
+def get_morpheme_features(morpheme: Morpheme) -> MorphemeFeatures:
+    pos = morpheme.part_of_speech()
+    return pos[0], pos[1], pos[2], pos[3]
 
 
-class StateMachine(object):
-    def __init__(self, states, transitions, initial_state, default_state=None):
+class StateMachine:
+
+    def __init__(
+            self,
+            states: List[State],
+            initial_state: State,
+            default_state: State,
+            transitions: List[StateTransitionRule]
+    ):
         self._states = frozenset(states)
-
         self._transitions = {}
+
         for src, features, dst in transitions:
             assert src is None or src in self._states
             assert dst in self._states
-            assert features is not None
             self._transitions[(src, features)] = dst
 
         assert initial_state in self._states
         self._initial_state = initial_state
 
-        assert default_state is None or default_state in self._states
+        assert default_state in self._states
         self._default_state = default_state
 
-    def get_next_state(self, src_state, node):
+    def get_next_state(self, src_state: State, morpheme: Morpheme):
+        features = get_morpheme_features(morpheme)
 
-        features = node_features(node)
-
+        # 1) transition from specific source state
         if (src_state, features) in self._transitions:
-            dst_state = self._transitions[(src_state, features)]
+            return self._transitions[(src_state, features)]
+        # 2) wildcard transition from any source state
         elif (None, features) in self._transitions:
-            dst_state = self._transitions[(None, features)]
+            return self._transitions[(None, features)]
+        # 3) fallback to default state
         else:
-            dst_state = self._default_state
+            return self._default_state
 
-        return dst_state
-
-    def run(self, inputs: Iterable[Morpheme]):
-        context = []
-
+    def run(self, context: Any, morphemes: Iterable[Morpheme]):
         current_state = self._initial_state
 
-        for node in inputs:
-            next_state = self.get_next_state(current_state, node)
-            next_state.on_input(context, node)
+        for m in morphemes:
+            next_state = self.get_next_state(current_state, m)
+            next_state.on_input(context, m)
             current_state = next_state
 
         return context
-
-
-def create_fsm():
-    return StateMachine(
-        [
-            UNSPECIFIED_WORD_STATE,
-            VERB_STATE,
-            VERB_INFLECTION_STATE,
-            ADJECTIVE_STATE,
-            ADJECTIVE_INFLECTION_STATE
-        ],
-        [
-            # --- transitions to aggregate inflected verbs ---
-
-            # start of a verb
-            (None, ('動詞', '一般', '*', '*'), VERB_STATE),
-            # Some verbs (for example 見る) are detected as non independent even when standing alone,
-            # so we need to take this into account well for starting a new verb.
-            # However this wildcard state is overridden by a more specific state so that we don't accidentally
-            # break an inflected verb apart.
-            (None, ('動詞', '非自立可能', '*', '*'), VERB_STATE),
-            # The inflected part of a verb is started by either an auxiliary verb, ...
-            (VERB_STATE, ('助動詞', '*', '*', '*'), VERB_INFLECTION_STATE),
-            # or a conjunctive particle, i.e. て or で.
-            (VERB_STATE, ('助詞', '接続助詞', '*', '*'), VERB_INFLECTION_STATE),
-            # continue aggregating any auxiliary verbs ...
-            (VERB_INFLECTION_STATE, ('助動詞', '*', '*', '*'), VERB_INFLECTION_STATE),
-            # and non independent verbs, e.g. the いる　of the continuous form
-            (VERB_INFLECTION_STATE, ('動詞', '非自立可能', '*', '*'), VERB_INFLECTION_STATE),
-
-            # suffix for seeming/looks-like
-            (VERB_STATE, ('形状詞', '助動詞語幹', '*', '*'), VERB_INFLECTION_STATE),
-
-            # --- transitions to aggregate inflected adjectives ---
-
-            # start of an adjective
-            (None, ('形容詞', '一般', '*', '*'), ADJECTIVE_STATE),
-            # can be followed by negating suffix
-            (ADJECTIVE_STATE, ('形容詞', '非自立可能', '*', '*'), ADJECTIVE_INFLECTION_STATE),
-            # and/or suffix for the past-tense
-            (ADJECTIVE_STATE, ('助動詞', '*', '*', '*'), VERB_INFLECTION_STATE),
-            (ADJECTIVE_INFLECTION_STATE, ('助動詞', '*', '*', '*'), VERB_INFLECTION_STATE),
-
-            # suffix for seeming/looks-like
-            (ADJECTIVE_STATE, ('形状詞', '助動詞語幹', '*', '*'), ADJECTIVE_INFLECTION_STATE)
-        ],
-        UNSPECIFIED_WORD_STATE,
-        UNSPECIFIED_WORD_STATE
-    )
