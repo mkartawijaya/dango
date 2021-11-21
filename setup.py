9cc9d498c4eac851d6baa96ced528c1d91a87216
Martin Kartawijaya <git@m.kartawijaya.dev>
2021-11-21 19:41:04 +0100

    Bump version for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     setup(
         name='dango',
-        version='0.0.1a',
+        version='0.0.1',
         description='An easy to use tokenizer for Japanese text, aimed at language learners and non-linguists',
         long_description=fh.read(),
         long_description_content_type="text/markdown",
